7a3a7cdb5602a30b5a24effadc6f0c8ac86130e1
waog <oliver.stadie@gmail.com>
2020-02-16 18:33:50 +0100

    ADD static TODO list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Waog!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODOs:</h1>
+      <button>🔄</button>
+      <ol>
+        <li>
+          clean up <button>❌</button>
+        </li>
+        <li>
+          do homework <button>❌</button>
+        </li>
+        <li>
+          <input type="text" />
+          <button>Add</button>
+        </li>
+      </ol>
     </div>
   );
-}
+};
 
 export default App;
